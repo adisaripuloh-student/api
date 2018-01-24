6c4ca599b58f17e076b5989fab562cba536465cd
Adi Saripuloh <adisaripuloh@gmail.com>
2018-01-24 12:19:37 +0700

    add route login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#landing'
 
+  post 'login', to: 'sessions#new'
+  delete 'logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
