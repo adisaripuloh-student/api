b1dd830612f580c975b25114d44393f8b5c3fac6
Adi Saripuloh <adisaripuloh@gmail.com>
2018-01-24 14:33:33 +0700

    add login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+  def new
+    if logged_in?
+      redirect_to root_path
+    end
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash[:danger] = "username dan password tanya ke adisaripuloh@gmail.com"
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#landing'
 
-  post 'login', to: 'sessions#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
